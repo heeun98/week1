da81405794ecdb89b366fa7eaf9de700459b3886
Chuseok22 <bjh59629@naver.com>
2025-03-06 16:15:03 +0900

    Update Student.java

--- src/main/java/com/example/week1/git/Student.java
+++ src/main/java/com/example/week1/git/Student.java
@@ -1,17 +1,14 @@
 package com.example.week1.git;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Student {
 
   private String name;
   private int age;
-
-  public Student(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
 
   public static void main(String[] args) {
     Student student1 = new Student("홍길동", 25);
