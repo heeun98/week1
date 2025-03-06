c6cf92b33e1e71fde576521d43e6a263f08e1ce7
Chuseok22 <bjh59629@naver.com>
2025-03-06 16:07:58 +0900

    init

--- src/main/java/com/example/week1/Week1Application.java
+++ src/main/java/com/example/week1/Week1Application.java
@@ -2,7 +2,6 @@ package com.example.week1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 @SpringBootApplication
 public class Week1Application {
@@ -10,7 +9,5 @@ public class Week1Application {
   public static void main(String[] args) {
     SpringApplication.run(Week1Application.class, args);
 
-    Student student1 = new Student("백지훈", 25);
-    System.out.println("이름: " + student1.getName() + ", 나이: " + student1.getAge());
   }
 }
